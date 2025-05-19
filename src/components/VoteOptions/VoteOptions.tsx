99d50641cac4ab1463e1325de474e106e5ec07d9
Сергій Власов <vikinglaurasun@gmail.com>
2025-05-19 17:45:26 +0300

    fix VoteOption props

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,13 +1,13 @@
 import css from './VoteOptions.module.css'
 import type { VoteType } from '../../types/votes'
 
-interface Props {
+interface VoteOptionsProps {
   onVote: (type: VoteType) => void
   onReset: () => void
   canReset: boolean
 }
 
-export default function VoteOptions({ onVote, onReset, canReset }: Props) {
+export default function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
   return (
     <div className={css.container}>
       <button className={css.button} onClick={() => onVote('good')}>Good</button>
